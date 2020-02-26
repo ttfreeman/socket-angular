1ddff988a5019b0e94b2510625017649437bb857
Tony <36289053+ttfreeman@users.noreply.github.com>
2020-02-26 14:03:02 -0700

    initial commit

---
+++ socket-app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { DocumentListComponent } from "./document-list/document-list.component";
+import { DocumentComponent } from "./document/document.component";
+
+import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+const config: SocketIoConfig = { url: "http://localhost:4444", options: {} };
+
+@NgModule({
+  declarations: [AppComponent, DocumentListComponent, DocumentComponent],
+  imports: [BrowserModule, FormsModule, SocketIoModule.forRoot(config)],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
